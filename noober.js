1867fa1aca49682db0d34bbdb669c30b7a28b57a
wesley-howell <wesley.howell@kellogg.northwestern.edu>
2021-04-19 21:01:11 -0500

    a

--- noober.js
+++ noober.js
@@ -12,6 +12,16 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   // 🔥 YOUR CODE GOES HERE 🔥
   // Write the recipe (algorithm) in the comments. Then, write the code.
+  // Goal: a program that parses the passenger data output by Noober's random ride generator, by defining each data point as an understandable variable, 
+  // storing each of the variables and subsequent string outputs in memory.
+  // then use these variables to display a human-readable output in the JavaScript console, 
+  // providing the driver with all necessary information to successfully provide a ride service.
+
+  // parse passenger details into variables for name and phone number, and store them in memory
+  let passengerFirstName = ride.passengerDetails.first 
+
+  console.log(passengerFirstName)
   
+
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
